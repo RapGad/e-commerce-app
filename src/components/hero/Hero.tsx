84e3ba50e2f704e63d902d4c63ed50a2305b535e
rapgad <jeffreyhayford98@gamil.com>
2025-09-18 16:16:22 +0000

    Fixing mobile view for hero section

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,43 +1,53 @@
-import React from 'react'
-import Image from 'next/image'
-import styles from './page.module.css'
+import React from "react";
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Button } from "../ui/button";
+import { Flame } from "lucide-react";
 const Hero = () => {
   return (
     <div className={styles.heroContainer}>
-        <div className={styles.imageContainer}>
-            <span style={{"--i": 1 }as React.CSSProperties}>
-                <Image className={styles.img} width={1000} height={1000} src="https://images.unsplash.com/photo-1757416654883-c73c67b3382b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" alt=''/>
-            </span>
-            <span style={{"--i": 2 }as React.CSSProperties}>
-                <Image className={styles.img} width={1000} height={1000} src="https://images.unsplash.com/photo-1757416654883-c73c67b3382b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" alt=''/>
-            </span>
-            <span style={{"--i": 3 }as React.CSSProperties}>
-                <Image className={styles.img} width={1000} height={1000} src="https://images.unsplash.com/photo-1757416654883-c73c67b3382b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" alt=''/>
-            </span>
-            <span style={{"--i": 4 }as React.CSSProperties}>
-                <Image className={styles.img} width={1000} height={1000} src="https://images.unsplash.com/photo-1757416654883-c73c67b3382b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" alt=''/>
-            </span>
-            <span style={{"--i": 5 }as React.CSSProperties}>
-                <Image className={styles.img} width={1000} height={1000} src="https://images.unsplash.com/photo-1757416654883-c73c67b3382b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" alt=''/>
-            </span>
-            <span style={{"--i": 6 }as React.CSSProperties}>
-                <Image className={styles.img} width={1000} height={1000} src="https://images.unsplash.com/photo-1757416654883-c73c67b3382b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" alt=''/>
-            </span>
-            <span style={{"--i": 7 }as React.CSSProperties}>
-                <Image className={styles.img} width={1000} height={1000} src="https://images.unsplash.com/photo-1757416654883-c73c67b3382b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" alt=''/>
-            </span>
-            <span style={{"--i": 8 }as React.CSSProperties}>
-                <Image className={styles.img} width={1000} height={1000} src="https://images.unsplash.com/photo-1757416654883-c73c67b3382b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" alt=''/>
-            </span>
+      <div className={styles.topHeroContainer}>
+        <div className={styles.topLeftHeroContainer}>
+            <div className={styles.boxDesignerTop}></div>
+            <div className={styles.imgContainerTop}>
+                <Image className={styles.img} src="/img3.png" alt="hero" width={400} height={400}/>
 
+            </div>
         </div>
 
-        <div className={styles.auction}>
+        <div className={styles.topRightHeroContainer}>
+            <div className={styles.textContainer}>
+                <h1>Join Our <span>Ongoing Auction</span> Now!!!</h1> 
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
+                <p>Pay Less Win Big</p>
+
+            </div>
+            <div className={styles.buttonContainer}>
+            <Button className={styles.btn}> <Flame stroke="none" color="red" fill="currentColor" size={89} className={`${styles.animateFLicker} mr-2 h-4 w-4 text-orange-500`} /> Join now!!</Button>
+
+            </div>
+        </div>
+      </div>
+      <div className={styles.bottomHeroContainer}>
+        <div className={styles.bottomLeftHeroContainer}>
+          <div className={styles.cardDesign}></div>
+          <div className={styles.cardDesign}></div>
+          <div className={styles.cardDesign}></div>
+          <div className={styles.cardDesign}></div>
+          <div className={styles.cardDesign}></div>
+            
+        </div>
+        <div className={styles.bottomRightHeroContainer}>
+          <div className={styles.boxDesignerBottom}></div>
+            <div className={styles.imgContainerBottom}>
+                <Image className={styles.img} src="/img1.png" alt="hero" width={400} height={400}/>
+            </div>
 
         </div>
-      
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
